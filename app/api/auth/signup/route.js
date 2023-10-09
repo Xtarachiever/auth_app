80469c10d75fdea26459b98aa5e325558968e2d0
sther <estheradeyemi796@gmail.com>
2023-10-09 18:09:33 +0100

    Minor

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -6,9 +6,15 @@ import { hash } from 'bcryptjs';
 export async function POST(req,res){
     connectMongo().catch(error => res.json({ error: "Connection Failed...!"}))
 
-    // only post method is accepted
-    // if(req.method === 'POST'){
 
+    // only post method is accepted
+    if(req.method === 'POST'){
+
+    if(!req.body) return new Response(JSON.stringify({ message: "Don't have form data...!"}),{
+        status:404
+    });
+
+        
     //     if(!req.body) return res.status(404).json({ error: "Don't have form data...!"});
     //     const { username, email, password } = req.body;
 
@@ -24,6 +30,6 @@ export async function POST(req,res){
 
     // } else{
     //     res.status(500).json({ message: "HTTP method not valid only POST Accepted"})
-    // }
+    }
 
 }
